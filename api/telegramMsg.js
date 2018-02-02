9db77d7c96243bc5588508d648aacd618a9cbdfa
Roman Turusov <borka.grr@gmail.com>
2018-02-02 21:09:17 +0300

    init and some logic added

---
+++ api/telegramMsg.js
@@ -0,0 +1,29 @@
+module.exports.sendMsg = (req, res) => {
+  const config = require('../../config/config.json');
+  let http = require('request')
+  let reqBody = req.body
+  let fields = [
+    '<b>Name</b>: ' + reqBody.name,
+    '<b>Email</b>: ' + reqBody.email,
+    reqBody.text
+  ]
+  let msg = ''
+  fields.forEach(field => {
+    msg += field + '\n'
+  });
+  console.log(msg)
+  msg = encodeURI(msg)
+  console.log(msg)
+  http.post(`https://api.telegram.org/bot${config.telegram.token}/sendMessage?chat_id=${config.telegram.chat}&parse_mode=html&text=${msg}`, function (error, response, body) {
+    console.log('error:', error); 
+    console.log('statusCode:', response && response.statusCode); 
+    console.log('body:', body); 
+    if(response.statusCode===200){
+      res.status(200).json({status: 'ok', message: 'Успешно отправлено!'});
+    }
+    if(response.statusCode===400){
+      res.status(400).json({status: 'error', message: 'Произошла ошибка!'});
+    }
+  });
+
+}
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+const ctrlTelegram = require('../api/telegramMsg');
+
+router.post('/telegram', ctrlTelegram.sendMsg);
+
+module.exports = router;
